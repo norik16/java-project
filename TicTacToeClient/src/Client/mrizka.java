3559a699a147f35267c3131d92ca0192df096719
xandel <xandel@vyt8.lan.luzanky.cz>
2015-03-30 18:38:29 +0200

    interface mrizka

---
+++ TicTacToeClient/src/Client/mrizka.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Client;
+
+/**
+ *
+ * @author xandel
+ */
+public interface mrizka {
+   
+    public int getsize();
+    
+    public char zjistitZnak(int x,int y);
+    
+    public void vypsatMrizku();
+    
+    public void aktualizaceMrizky();
+    
+    
+    
+}
---
+++ TicTacToeClient/src/Client/mrizkaImpl.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Client;
+
+/**
+ *
+ * @author xandel
+ */
+public class mrizkaImpl implements mrizka {
+
+    @Override
+    public int getsize() {
+        return 0;
+        
+    }
+
+    @Override
+    public char zjistitZnak(int x, int y) {
+        return 0;
+        
+    }
+
+    @Override
+    public void vypsatMrizku() {
+        
+    }
+
+    @Override
+    public void aktualizaceMrizky() {
+        
+    }
+    
+}
