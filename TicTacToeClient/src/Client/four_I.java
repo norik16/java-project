8cb8e3e8b0df00068d53e2fe282c1809e13076d5
luc <luc@pavel-soukromy.lan.luzanky.cz>
2015-04-13 18:31:25 +0200

    ActualState Class add and point

---
+++ TicTacToeClient/src/Client/four_I.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Client;
+
+/**
+ *
+ * @author luc
+ */
+public class four_I {
+    point Point1 = point(0,0);
+
+
+}
