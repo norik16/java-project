8fe5877c8816b9c9e5bbd9bb35b2ddd243dbbc97
xandel <xandel@vyt8.lan.luzanky.cz>
2015-04-27 18:25:19 +0200

    zjistovani ctverice

---
+++ TicTacToeClient/src/Client/point.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Client;
+
+/**
+ *
+ * @author xandel
+ */
+
+
+
+
+public class point {
+    private int x;
+    private int y;
+
+    public point(int x, int y) {
+        this.x=x;
+        this.y=y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+    
+    
+}
